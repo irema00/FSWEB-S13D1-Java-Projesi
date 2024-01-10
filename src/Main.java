20e55dc52446789aed53c661c5512a11e867e279
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-10 19:16:22 +0300

    adds shouldWakeUp method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+        System.out.println(shouldWakeUp (true, 1));
+        System.out.println(shouldWakeUp (false, 2));
+        System.out.println(shouldWakeUp (true, 8));
+        System.out.println(shouldWakeUp (true, -1));
+    public static boolean shouldWakeUp(Boolean isBarking , int time ) {
+        if (time < 0 || time > 23) {
+            return false;
+        } else if(time <8 || time >20) {
+            return isBarking;
+        }
+    return false;
     }
 }
