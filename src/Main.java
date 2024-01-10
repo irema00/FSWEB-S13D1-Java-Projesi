f4f4af59c55326e98f0e85f05598642a10c7d997
İrem Akın Tunga <iremakin00@gmail.com>
2024-01-10 19:21:19 +0300

    adds hasTeen method

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println(shouldWakeUp (false, 2));
         System.out.println(shouldWakeUp (true, 8));
         System.out.println(shouldWakeUp (true, -1));
+        System.out.println("--------");
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
     public static boolean shouldWakeUp(Boolean isBarking , int time ) {
         if (time < 0 || time > 23) {
             return false;
@@ -14,5 +18,8 @@ public class Main {
             return isBarking;
         }
     return false;
+    }
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        return num1 >= 13 && num1 <= 19 || num2 >= 13 && num2 <= 19 || num3 >= 13 && num3 <= 19;
     }
 }
